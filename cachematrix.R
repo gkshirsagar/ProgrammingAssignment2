b1690a4efcc9cbe27b3c6fc1179ad97a221441ec
unknown <gkshirsagar@users.noreply.github.com>
2014-04-27 03:00:47 +0530

    Implemented makeCacheMatrix() and cacheSolve() functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+##
+## Implements a special matrix that can cache its inverse for speedy lookup.
+##
+## Usage:
+## Call 'makeCacheMatrix()' to create a new matrix.
+## Call 'cacheSolve()' to get the matrix's inverse.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix()
+## Creates the special matrix.
+## Pass a regular square matrix in the call to create this matrix.
+makeCacheMatrix <- function(m = matrix()) {
+  # for caching the matrix's inverse
+  i <- NULL
 
+  # sets this matrix to a new matrix; invalidates the inverse
+  set <- function(p) {
+    m <<- p
+    i <<- NULL
+  }
+
+  # return the underlying bare matrix
+  get <- function() m
+
+  # set the inverse of this matrix
+  setSolve <- function(s) i <<- s
+
+  # return the cached inverse of this matrix
+  getSolve <- function() i
+
+  # return a list disguised as the special matrix
+  list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve()
+## Computes the inverse of the special matrix created by makeCacheMatrix()
+## and also stores the inverse back into the matrix for caching.
+cacheSolve <- function(m, ...) {
+  # get the cached inverse and see if it is valid
+  i <- m$getSolve()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!is.null(i)) {
+    # it is valid so return it
+    message("getting cached data")
+    return(i)
+  }
+
+  # if the inverse is not valid, compute it using the actual matrix
+  data <- m$get()
+  i <- solve(data, ...)
+
+  # cache it
+  m$setSolve(i)
+
+  # and return the inverse
+  i
 }
